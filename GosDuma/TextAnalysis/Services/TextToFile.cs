2289668f67e5919d2f190a1a23d6c9c022bf767b
Якутин Артем <artem-av-st@yandex.ru>
2017-10-14 11:59:31 +0700

    Added class Analyzer which performs manipulations with the text

---
+++ GosDuma/TextAnalysis/Services/TextToFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysis.Services
+{
+    public static class TextToFile
+    {
+        public static void SaveTextToFile(string text, string path, string fileName, FileMode fileMode=FileMode.Create)
+        {
+            using (FileStream fstream = new FileStream(Path.Combine(path, fileName), fileMode))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+    }
+}
