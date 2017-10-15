8a171708f8ce2f5be83c1983f8a26bdafb6c1662
Якутин Артем <artem-av-st@yandex.ru>
2017-10-15 22:04:25 +0700

    Added features to Analyzer class

---
+++ GosDuma/TextAnalysis/Model/MystemResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysis.Model
+{  
+
+    public class MystemResult
+    {
+        public string Text { get; set; }
+        public List<Analysis> AnalysisList { get; set; }
+
+        public class Analysis
+        {
+            public string Lex { get; set; }
+            public string Gr { get; set; }
+        }
+    }
+}
